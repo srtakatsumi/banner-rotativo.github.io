7e20c4ab0ca3d4f7678c932b6abd828356c7b5f9
Victoria Gabriella <73000702+srtakatsumi@users.noreply.github.com>
2021-07-15 21:10:10 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -25,3 +25,25 @@ function start() {
 }
 
 window.addEventListener("load", start)
+
+function searchProducts() {
+    const input = document.getElementById('filter').value.toUpperCase();
+    const cardssContainer = document.getElementById('cards-lists');
+    console.assert(cardssContainer);
+
+    const cards = cardsContainer.getElementById('cards');
+    console.log(cards);
+
+    for (let i = 0; i < cards.length; i++) {
+        let title = cards[i].querySelector(".cards-body h3.cards-title");
+        console.log(title);
+
+
+        if (title.innerText.toUpperCase().indexOf(input) > -1) {
+            cards[i].style.display = "";
+        } else {
+            cards[i].style.display = "none";
+        }
+
+    }
+}
